06fd54b4c806ea3b7f3dca51fcf26e7020b922ee
LeeW <cl87654321@gmail.com>
2018-03-10 01:44:56 +0800

    Add test cases for stock trader

--- tests/test_corrector.py
+++ tests/test_corrector.py
@@ -1,8 +1,8 @@
 import pytest
 import pandas as pd
 
-from corrector import (
-    calculate_profit, check_stock_actions_length,
+from profit_calculator import (
+    calculate_profit, check_stock_actions_length, StockTrader,
     InvalidActionError, StockNumExceedError, InvalidActionNumError
 )
 
@@ -67,8 +67,78 @@ class TestCheckStockActionsLength:
     def test_valid_length(self, stocks, init_actions):
         assert check_stock_actions_length(stocks, init_actions) is True
 
-    def test_valid_length(self, stocks, init_actions):
+    def test_invalid_length(self, stocks, init_actions):
         actions = init_actions
         actions.append(0)
 
         assert check_stock_actions_length(stocks, init_actions) is False
+
+
+class TestStockTrader:
+    @pytest.fixture(scope='function')
+    def stock_tracker(self):
+        return StockTrader()
+
+    def test_is_not_holding_stock(self, stock_tracker):
+        assert stock_tracker.is_holding_stock is False
+
+    def test_is_holding_stock(self, stock_tracker):
+        stock_tracker.holding_price = 0
+
+        assert stock_tracker.is_holding_stock is True
+
+    def test_is_not_shorting_stock(self, stock_tracker):
+        assert stock_tracker.is_holding_stock is False
+
+    def test_is_shorting_stock(self, stock_tracker):
+        stock_tracker.sell_short_price = 0
+
+        assert stock_tracker.is_holding_stock is False
+
+    def test_buy_when_not_holding(self, stock_tracker):
+        stock_tracker.buy(10)
+
+        assert stock_tracker.holding_price == 10
+        assert stock_tracker.is_holding_stock is True
+        assert stock_tracker.is_shorting_stock is False
+        assert stock_tracker.accumulated_profit == 0
+
+    def test_sell_when_not_shorting(self, stock_tracker):
+        stock_tracker.sell(15)
+
+        assert stock_tracker.sell_short_price == 15
+        assert stock_tracker.is_holding_stock is False
+        assert stock_tracker.is_shorting_stock is True
+        assert stock_tracker.accumulated_profit == 0
+
+    def test_buy_when_shorting_stock(self, stock_tracker):
+        stock_tracker.sell(15)
+        stock_tracker.buy(10)
+
+        assert stock_tracker.is_holding_stock is False
+        assert stock_tracker.is_shorting_stock is False
+        assert stock_tracker.accumulated_profit == 5
+
+    def test_buy_when_holding_stock(self, stock_tracker):
+        stock_tracker.buy(10)
+
+        with pytest.raises(StockNumExceedError) as err_info:
+            stock_tracker.buy(15)
+
+        assert err_info.value.args[0] == 'You cannot buy stocks when you hold one'
+
+    def test_sell_when_holding_stock(self, stock_tracker):
+        stock_tracker.buy(10)
+        stock_tracker.sell(15)
+
+        assert stock_tracker.is_holding_stock is False
+        assert stock_tracker.is_shorting_stock is False
+        assert stock_tracker.accumulated_profit == 5
+
+    def test_sell_when_shorting_stock(self, stock_tracker):
+        stock_tracker.sell(10)
+
+        with pytest.raises(StockNumExceedError) as err_info:
+            stock_tracker.sell(15)
+
+        assert err_info.value.args[0] == "You cannot sell short stocks when you've already sell short one"
